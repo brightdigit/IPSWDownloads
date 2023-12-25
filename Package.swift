df664a18fa532eaa67fe6e970dd70e6e74abf863
Leo Dion <leogdion@brightdigit.com>
2023-12-25 14:08:35 -0500

    working on adding ipswme

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "IPSW",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
-        dependencies: [
-            .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
-            .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
-            .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
-        ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "IPSW",
-            dependencies: [
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-            ]
-        ),
-    ]
+  name: "IPSW",
+  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "IPSW",
+      dependencies: [
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession")
+      ]
+    )
+  ]
 )
--- Sources/IPSW/MainApp.swift
+++ Sources/IPSW/MainApp.swift
@@ -1,39 +1,30 @@
-//import OpenAPIRuntime
-//import OpenAPIURLSession
 //
-//
-//
-//
-//
+// MainApp.swift
+// Copyright (c) 2023 BrightDigit.
 //
 
 import OpenAPIURLSession
 
-
 @main
-struct MainApp {
-   static func main() async throws {
-      let client = try Client(serverURL: Servers.server1(), transport: URLSessionTransport())
-      let response = try await client.firmwaresForDevice(.init(path: .init(identifier:  "VirtualMac2,1"), query: .init(_type: "ipsw")))
-     guard let ipswFiles = try response.ok.body.json.firmwares else {
-       return
-     }
-     let result = try await withThrowingTaskGroup(of: Optional<Components.Schemas.Firmware>.self) { group in
-       for ipswFile in ipswFiles {
-         group.addTask {
-           
-           return try await  client.ipswByIdentifierAndBuild(.init(path: .init(identifier: "VirtualMac2,1", buildid: ipswFile.buildid))).ok.body.json
-         }
-       }
-       
-       return try await group.compactMap{$0}.reduce(into: [Components.Schemas.Firmware]()) { partialResult, item in
-         partialResult.append(item)
-       }
-     }
-     
-     dump(result)
-     
-     
-     
+enum MainApp {
+  static func main() async throws {
+    let client = try Client(serverURL: Servers.server1(), transport: URLSessionTransport())
+    let response = try await client.firmwaresForDevice(.init(path: .init(identifier: "VirtualMac2,1"), query: .init(_type: "ipsw")))
+    guard let ipswFiles = try response.ok.body.json.firmwares else {
+      return
     }
+    let result = try await withThrowingTaskGroup(of: Components.Schemas.Firmware?.self) { group in
+      for ipswFile in ipswFiles {
+        group.addTask {
+          try await client.ipswByIdentifierAndBuild(.init(path: .init(identifier: "VirtualMac2,1", buildid: ipswFile.buildid))).ok.body.json
+        }
+      }
+
+      return try await group.compactMap { $0 }.reduce(into: [Components.Schemas.Firmware]()) { partialResult, item in
+        partialResult.append(item)
+      }
+    }
+
+    dump(result)
+  }
 }
