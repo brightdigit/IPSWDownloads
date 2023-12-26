8796ec0e8343fbfe16155feae8688c63b6cca465
Leo Dion <leogdion@brightdigit.com>
2023-12-26 17:25:38 -0500

    setting up ci

---
+++ Scripts/generate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+swift run swift-openapi-generator generate --output-directory Sources/IPSWDownloads --config Sources/IPSWDownloads/openapi-generator-config.yaml Sources/IPSWDownloads/openapi.yaml
