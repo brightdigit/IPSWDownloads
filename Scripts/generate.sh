87ab1f115f871f4a0e7537a8f9ee8aed4270b9fe
Leo Dion <leogdion@brightdigit.com>
2024-01-11 17:20:40 -0500

    refining API

--- Scripts/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-swift run swift-openapi-generator generate --output-directory Sources/IPSWDownloads --config Sources/IPSWDownloads/openapi-generator-config.yaml Sources/IPSWDownloads/openapi.yaml
---
+++ Sources/IPSWDownloads/IPSWDownloads.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leo Dion on 1/11/24.
+//
+import OpenAPIRuntime
+import Foundation
+
+/// A hand-written Swift API for the greeting service, one that doesn't leak any generated code.
+public struct IPSWDownloads {
+  
+  public static let serverURL = try! Servers.server1()
+
+    /// The underlying generated client to make HTTP requests to GreetingService.
+    private let underlyingClient: any APIProtocol
+//
+    /// An internal initializer used by other initializers and by tests.
+    /// - Parameter underlyingClient: The client to use to make HTTP requests.
+    private init(underlyingClient: any APIProtocol) { self.underlyingClient = underlyingClient }
+//
+//    /// Creates a new client for GreetingService.
+  public init(serverURL: URL = Self.serverURL, transport: any ClientTransport) {
+        self.init(
+            underlyingClient: Client(
+                serverURL: serverURL,
+                transport: transport
+            )
+        )
+    }
+//
+//    /// Fetches the customized greeting for the provided name.
+//    /// - Parameter name: The name for which to provide a greeting, or nil to get a default.
+//    /// - Returns: A customized greeting message.
+//    /// - Throws: An error if the underlying HTTP client fails.
+//    public func getGreeting(name: String?) async throws -> String {
+//        let response = try await underlyingClient.getGreeting(query: .init(name: name))
+//        return try response.ok.body.json.message
+//    }
+}
