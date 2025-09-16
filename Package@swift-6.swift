7542c064451fbcafabdd860ec96a544544684987
leogdion <leogdion@brightdigit.com>
2025-09-16 21:03:44 +0000

    updating more code [skip ci]

--- Package@swift-6.swift
+++ Package@swift-6.swift
@@ -44,7 +44,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/apple/swift-openapi-runtime",
-      from: "1.8.0"
+      from: "1.8.2"
     ),
     .package(
       url: "https://github.com/apple/swift-openapi-urlsession",
